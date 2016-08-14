2b9f63d493f87ce40d11c1f600e3ccc7876b5e12
Yinghong Liao <382112699@qq.com>
2016-08-14 21:44:33 +0800

    Circular List and Solution to the Josephus problem
    Josephus problem
    People are standing in a circle waiting to be executed. Counting begins at a specified point in the circle and proceeds around the circle in a specified direction. After a specified number of people are skipped, the next person is executed. The procedure is repeated with the remaining people, starting with the next person, going in the same direction and skipping the same number of people, until only one person remains, and is freed. (source : https://en.wikipedia.org/wiki/Josephus_problem)

---
+++ CircList/CircList.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "LinearList.h"
+template <class T>
+struct CircLinkNode {
+	T data;
+	CircLinkNode<T> * link;
+	CircLinkNode(CircLinkNode<T> * next = NULL) : link(next) {}
+	CircLinkNode(T d, CircLinkNode<T> * next = NULL) : data(d), link(next) {}
+}
+
+template <class T>
+class CircList : public LinearList<T> {
+public:
+	CircList(const T& x);
+	CircList(CircList<T>& L);
+	~CircList();
+	int Length() const;
+	bool IsEmpty() {return first->link == first ? true : false;}
+
+	CircLinkNode<T> * getHead() const;
+	void setHead(CircLinkNode<T> * p);
+	CircLinkNode<T> * Search(T x);
+	CircLinkNode<T> * Locate(int i);
+	T& getData(int i) const;
+	void setData(int i, T& x);
+	bool Insert(int i, T& x);
+	bool Remove(int i, T& x);
+private:
+	CircLinkNode<T> * first, * last;
+};
